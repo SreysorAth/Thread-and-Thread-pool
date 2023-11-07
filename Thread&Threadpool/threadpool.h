c23a54d920a2604e5b3ae12287d320d91a0d5a5b
SreysorAth <147776796+SreysorAth@users.noreply.github.com>
2023-11-07 22:18:18 +0700

    Add files via upload

---
+++ Thread&Threadpool/threadpool.h
@@ -0,0 +1,25 @@
+#include <pthread.h>
+#include <semaphore.h>
+
+void execute(void (*somefunction)(void *p), void *p);
+int pool_submit(void (*somefunction)(void *p), void *p);
+void *worker(void *param);
+void pool_init(void);
+void pool_shutdown(void);
+
+struct task
+{
+    void (*function)(void *p,int thread_id);
+    void *data;
+    struct task *next;
+    int task_id; //debug purpose
+};
+
+struct threadpool
+{
+    struct task *head;
+    struct task *tail;
+    pthread_mutex_t mutex;
+    sem_t semaphore;
+    int timespan;
+};
